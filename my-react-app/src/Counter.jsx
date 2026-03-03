323cbea48da97e7a5d9bbcd7a124684b4d42fc5a
Lokesh S M <smlokesh2006@gmail.com>
2026-03-03 22:48:54 +0530

    git test1

--- my-react-app/src/Counter.jsx
+++ my-react-app/src/Counter.jsx
@@ -5,7 +5,7 @@ function Counter(){
     return(
         <div>
             <h2>Counter: {count}</h2>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <button onClick={() => setCount(count + 1)}>demo</button>
             <button onClick={() => setCount(count - 1)}>Decrement</button>
         </div>
     );
